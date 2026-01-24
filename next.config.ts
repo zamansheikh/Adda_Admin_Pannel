fc769825a5a0a6135af696692fb923ed461dd7d5
zamansheikh <zaman6545@gmail.com>
2026-01-24 11:53:43 +0600

    Update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "http://88.222.213.235:3004",
     "http://88.222.213.235:8000",
+    "http://192.168.68.51:3004",
     "https://admin.zigoliveapp.xyz",
     "https://api.zigoliveapp.xyz",
     "http://admin.zigoliveapp.xyz",
